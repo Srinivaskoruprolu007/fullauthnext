383f5b917c314bf59700f1b62eba5154997e881f
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-17 22:56:40 +0530

    added profile page

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default function UserProfile({ params }: UserProfileParams) {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black/40">
+      <div className="container mx-auto max-w-md p-4 md:p-6 bg-white/10 rounded-lg shadow-lg">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold mb-4">Profile</h1>
+          <p className="text-lg">Welcome back, {params.id}!</p>
+          <p className="text-sm text-gray-400">{""}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
