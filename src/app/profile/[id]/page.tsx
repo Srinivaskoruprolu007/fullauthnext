da56236452e0e6536bfd8ec0c11b8d7680fb29de
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-18 23:57:22 +0530

    added dynamic page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,11 +1,13 @@
 export default async function UserProfile({
   params,
+}: {
+  params: Promise<{ id: string }>;
 }) {
-  const slug = (await params).slug;
+  const slug = (await params).id;
   console.log(slug);
   
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-700">
       <h1 className="text-5xl font-bold tracking-wide">Profile</h1>
       <hr className="w-1/2 my-6 border-gray-300" />
       <p className="text-4xl font-semibold tracking-wide">
