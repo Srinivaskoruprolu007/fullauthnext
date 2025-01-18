d96cbb6ca7bca369e97b794e36cf4595b34a6bcb
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-18 11:37:42 +0530

    updated some stylings in profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+import axios from "axios";
+import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (err) {
+      console.log("Failed to log out", err);
+      toast.error("Failed to log out");
+    }
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-black/40">
       <div className="container mx-auto max-w-md p-4 md:p-6 bg-white/10 rounded-lg shadow-lg">
@@ -6,6 +24,25 @@ export default function ProfilePage() {
           <h1 className="text-3xl font-bold mb-4">Profile</h1>
           <p className="text-lg">Welcome back, {""}!</p>
           <p className="text-sm text-gray-400">{""}</p>
+        </div>
+        <div className="text-center">
+          <button
+            onClick={logout}
+            className="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+          >
+            Logout
+          </button>
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              success: {
+                duration: 3000,
+              },
+              error: {
+                duration: 3000,
+              },
+            }}
+          />
         </div>
       </div>
     </div>
